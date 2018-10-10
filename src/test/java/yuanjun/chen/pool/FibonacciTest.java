81f22569436d8f0a3c2b1e247526ecc3291d5b88
kokomal <mattleblanc@live.cn>
2018-10-10 09:59:29 +0800

    fibonacci相关

---
+++ src/test/java/yuanjun/chen/pool/FibonacciTest.java
@@ -0,0 +1,103 @@
+/**
+ * @Title: FibonacciTest.java
+ * @Package: yuanjun.chen.pool
+ * @Description: TODO(用一句话描述该文件做什么)
+ * @author: 陈元俊
+ * @date: 2018年10月10日 上午9:26:14
+ * @version V1.0
+ * @Copyright: 2018 All rights reserved.
+ */
+package yuanjun.chen.pool;
+
+import java.math.BigDecimal;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
+import org.junit.Test;
+
+/**
+ * @ClassName: FibonacciTest
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @author: 陈元俊
+ * @date: 2018年10月10日 上午9:26:14
+ */
+public class FibonacciTest {
+
+    private static final BigDecimal ZERO = new BigDecimal(0);
+    private static final BigDecimal ONE = new BigDecimal(1);
+
+    @Test
+    public void testFibonacci() throws Exception {
+        int n = 44;
+        FibobacciTask fb = new FibobacciTask(n);
+        ForkJoinPool pool = ForkJoinPool.commonPool(); // 不太推荐用new，而是用工厂
+        BigDecimal k = pool.submit(fb).get();
+        System.out.println("ForkJoin Fibonacci(" + n + ") = " + k);
+        pool.awaitTermination(2, TimeUnit.SECONDS);
+        pool.shutdown();
+    }
+
+    @Test
+    public void testRecursiveFibonacci() {
+        int targ = 45;
+        System.out.println("Recursive Fibonacci(" + targ + ") = " + fiboRecursive(44));
+    }
+
+    @Test
+    public void testTailRecursiveFibonacci() {
+        int targ = 44;
+        System.out.println("Tail Fibonacci(" + targ + ") = " + smartFiboRecursive(targ, ZERO, ONE));
+    }
+    
+    @Test
+    public void testLoopFibonacci() {
+        int targ = 44;
+        System.out.println("Loop Fibonacci(" + targ + ") = " + loopFibonacci(44));
+    }
+
+    /**
+     * @Title: fiboRecursive
+     * @Description: 低效递归
+     * @return: BigDecimal
+     */
+    private static BigDecimal fiboRecursive(int n) {
+        if (n == 0) {
+            return ZERO;
+        } else if (n == 1) {
+            return ONE;
+        }
+        return fiboRecursive(n - 1).add(fiboRecursive(n - 2));
+    }
+
+    /**
+     * @Title: smartFiboRecursive
+     * @Description: 尾递归
+     * @return: BigDecimal
+     */
+    private static BigDecimal smartFiboRecursive(int n, BigDecimal fn_2, BigDecimal fn_1) {
+        if (n == 0) {
+            return fn_2;
+        }
+        return smartFiboRecursive(n - 1, fn_1, fn_2.add(fn_1));
+    }
+
+    /**
+     * @Title: loopFibonacci
+     * @Description: 纯迭代
+     * @return: BigDecimal
+     */
+    private static BigDecimal loopFibonacci(int n) {
+        BigDecimal f0 = ZERO;
+        BigDecimal f1 = ONE;
+        BigDecimal currentNum = ZERO;
+        if (n <= 1) {
+            return new BigDecimal(n);
+        }
+        for (int i = 2; i <= n; i++) {
+            currentNum = f0.add(f1);
+            f0 = f1;
+            f1 = currentNum;
+        }
+        return currentNum;
+    }
+
+}
--- src/test/java/yuanjun/chen/pool/ForkJoinPoolTest.java
+++ src/test/java/yuanjun/chen/pool/ForkJoinPoolTest.java
@@ -1,47 +1,35 @@
-/**  
- * @Title: ForkJoinPoolTest.java   
- * @Package: yuanjun.chen.pool   
- * @Description: TODO(用一句话描述该文件做什么)   
- * @author: 陈元俊     
- * @date: 2018年9月4日 下午1:18:15   
- * @version V1.0 
- * @Copyright: 2018 All rights reserved. 
+/**
+ * @Title: ForkJoinPoolTest.java
+ * @Package: yuanjun.chen.pool
+ * @Description: TODO(用一句话描述该文件做什么)
+ * @author: 陈元俊
+ * @date: 2018年9月4日 下午1:18:15
+ * @version V1.0
+ * @Copyright: 2018 All rights reserved.
  */
 package yuanjun.chen.pool;
 
-import java.math.BigDecimal;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.TimeUnit;
 import org.junit.Test;
 
-/**   
- * @ClassName: ForkJoinPoolTest   
- * @Description: TODO(这里用一句话描述这个类的作用)   
- * @author: 陈元俊 
- * @date: 2018年9月4日 下午1:18:15  
+/**
+ * @ClassName: ForkJoinPoolTest
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @author: 陈元俊
+ * @date: 2018年9月4日 下午1:18:15
  */
 public class ForkJoinPoolTest {
     
     @Test
     public void testSimplePrint() throws Exception {
-      PrintTask task = new PrintTask(0, 100);
-      //创建实例，并执行分割任务
-      ForkJoinPool pool = ForkJoinPool.commonPool(); // 不太推荐用new，而是用工厂
-      pool.submit(task);
-       //线程阻塞，等待所有任务完成
-      pool.awaitTermination(2, TimeUnit.SECONDS);
-      pool.shutdown();
-    }
-    
-    @Test
-    public void testFibonacci() throws Exception {
-        int n = 100;
-        FibobacciTask fb = new FibobacciTask(n);
+        PrintTask task = new PrintTask(0, 100);
+        // 创建实例，并执行分割任务
         ForkJoinPool pool = ForkJoinPool.commonPool(); // 不太推荐用new，而是用工厂
-        BigDecimal k = pool.submit(fb).get();
-        System.out.println("Fibonacci = " + k);
+        pool.submit(task);
+        // 线程阻塞，等待所有任务完成
         pool.awaitTermination(2, TimeUnit.SECONDS);
         pool.shutdown();
     }
-    
+
 }
